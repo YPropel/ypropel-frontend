420f0e8b4be1252a362d33af05d93bc786340a73
Rania Omar <raniaomar@Ranias-MacBook-Pro.attlocal.net>
2025-08-05 00:06:17 -0500

    Add company ield to jobs admin page

--- pages/CompanyDetails.tsx
+++ pages/CompanyDetails.tsx
@@ -39,7 +39,7 @@ const CompanyDetailsPage = () => {
     if (!companyId) return;
 
     // Redirect to PostJob page with the companyId as a query parameter
-    router.push(`/PostJob?companyId=${companyId}`);
+    router.push(`https://www.ypropel.com/PostJob?companyId=21`);
   };
 
   if (error) {
